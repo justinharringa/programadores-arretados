84c362983e45941a6465e46f5c109a4eeb80df79
Justin Harringa <justin@harringa.com>
2021-05-30 10:18:33 -0700

    Outro exemplo: funcao que removar elemento

--- exemplos/remover_item_da_lista_no_funcao.py
+++ exemplos/remover_item_da_lista_no_funcao.py
@@ -15,3 +15,19 @@ print(f"antes: {lista_de_opcoes}")
 outra_lista_retornado = tirar_da_lista(lista_de_opcoes)
 print(f"depois (original): {lista_de_opcoes}")
 print(f"depois (retornado): {outra_lista_retornado}")
+
+print("-----------outro exemplo-----------")
+outra_lista_de_opcoes = [1, 2, 3, 'a', 'b', 'c']
+
+
+def tirar_uma_coisa(lista, coisa_pra_remover):
+    lista.remove(coisa_pra_remover)
+    return lista
+
+
+print(f'lista original: {outra_lista_de_opcoes}')
+print(f'primeiro vamos ver o funcao retornar a mesma lista pra usar na hora')
+print(f'remove a: {tirar_uma_coisa(outra_lista_de_opcoes, "a")}')
+print(f'tb dar pra ver que removeu da lista original: {outra_lista_de_opcoes}')
+print(f'remove 1: {tirar_uma_coisa(outra_lista_de_opcoes, 1)}')
+print(f'remove c: {tirar_uma_coisa(outra_lista_de_opcoes, "c")}')
