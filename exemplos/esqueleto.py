53671b1d871a68558f7b5da434ae4d48848d6bf2
Justin Harringa <justin@harringa.com>
2021-10-20 14:49:08 -0700

    Adicionar exemploy do esqueleto

---
+++ exemplos/esqueleto.py
@@ -0,0 +1,25 @@
+esqueleto = '''
+      .-.
+     (o.o)
+      |=|
+     __|__
+   //.=|=.\\
+  // .=|=. \\
+  \\ .=|=. //
+   \\(_=_)//
+    (:| |:)
+     || ||
+     () ()
+     || ||
+     || ||
+l42 ==' '==
+'''
+
+
+# alguma passo
+### tem uma batalha com a pirata
+### vamos usar random pra ver se morre ou nao
+# outro passo
+
+
+print(esqueleto)
